b0b038d6097af201acb88c876702d83295e3caa5
Ahmed Abdullah <ahmed067abdullah@gmail.com>
2018-10-01 19:31:08 +0500

    insecure content bug fixed

--- src/config/helper.js
+++ src/config/helper.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getWeather = (city) => {
     return new Promise((resolve, reject) => {
-        axios.get(`http://api.apixu.com/v1/current.json?key=17a79ca8260f4d46805111508180110&q=${city}`)
+        axios.get(`https://api.apixu.com/v1/current.json?key=17a79ca8260f4d46805111508180110&q=${city}`)
             .then(res =>  resolve(res.data))
             .catch(err => reject("*Something went wrong"))
     })
